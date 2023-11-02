b7d10a524da7e6db281f74275726ee6d233458c0
Mohsen <mohsen@desap.co.uk>
2023-11-02 07:55:36 +0330

    Initial Commit

---
+++ Sources/LearningZonePackage/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//
+//
+//  Created by Mohsen on 02/11/2023.
+//
+
+import Foundation
+
+public struct SDK{
+    public static func doSomeWork(){
+        print("This is Learning zone package")
+        
+    }
+}
