92a4544345dab7202dfe52bb5fa40502a8822eee
Mohsen <mohsen@desap.co.uk>
2023-11-02 08:31:13 +0330

    third commit

--- Sources/LearningZonePackage/SDK.swift
+++ Sources/LearningZonePackage/SDK.swift
@@ -9,6 +9,6 @@ import Foundation
 
 public struct SDK{
     public static func doSomeWork(){
-        print("This is Learning zone package")
+        print("This is Learning zone package 0.0.3")
     }
 }
